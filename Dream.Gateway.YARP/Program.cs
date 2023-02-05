a323cadf97c9d8ca944774ef0471c105949b2e7a
dong <44517714+lurudong@users.noreply.github.com>
2023-02-05 14:59:55 +0800

    提交

--- Dream.Gateway.YARP/Program.cs
+++ Dream.Gateway.YARP/Program.cs
@@ -1,23 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 var app = builder.Build();
+app.UseRouting();
+// Register the reverse proxy routes
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.Urls.Add("http://localhost:5236");
+app.MapReverseProxy();
 app.Run();
